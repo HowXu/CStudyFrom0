2eaa79592bd7b8959b0421878d29d30ee3bb37be
HowXu <howxu366@outlook.com>
2022-12-22 21:36:45 +0800

    上传

---
+++ MyC/HelloWorld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>//导入一个文件 stdio.h st是一个标准库 i input o output 即标准输入输出库 .h头文件
+//<>表示导入系统文件 “”表示导入自定义文件
+//行注释
+/*
+多行注释
+*/
+//int数据类型 整型 出现在函数中表示函数的返回值、
+//main 程序的主函数，程序有且只有一个主函数
+int main2() {
+	printf("Hello World!\n");//这个是换行符
+	printf("你好C语言");
+	return 0;
+}
---
+++ MyC/SystemUsing.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>//导入system方法的头文件
+
+int main(){
+	printf("System调用成功！\n");
+	system("cmd");
+	return 0;
+}
